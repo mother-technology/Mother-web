41ef04dd740f21673c8a2ba8d285659fc9ef1cb4
tomasmckenna <tomasmckenna@users.noreply.github.com>
2020-11-20 11:39:57 +0100

    Added titles and descriptions, final final final edit version 6

--- src/App.tsx
+++ src/App.tsx
@@ -16,11 +16,11 @@ function App() {
             <div className="App">
                 <Menu />
                 <Switch>
-                    <Page exact path="/" component={Main} title="Mother." description="Digital therapeutics for mental healthcare." />
-                    <Page exact path="/about" component={About} title="ABOUT" description="B"/>
-                    <Page exact path="/collaboration" component={Collaboration} title="Collaboration" description="C"/>
-                    <Page exact path="/contact" component={Contact} title="Contact" description="D"/>
-                    <Page exact path="/privacy" component={Privacy} title="Privacy" description="D"/>
+                    <Page exact path="/" component={Main} title="Mother. - Bringing neuroscience to the people. | mother.technology" description="We are a hard-science deep-tech group of scientists and developers, using cutting-edge results from the lab to create digital therapeutics for mental health." />
+                    <Page exact path="/about" component={About} title="The Team. A diverse blend of scientists and engineers. | mother.technology" description="The background of the company and its members."/>
+                    <Page exact path="/collaboration" component={Collaboration} title="Collaboration - We are always open to working together. | mother.technology" description="Our company maintains a strong academic spirit and values collaboration with other research partners, on technologies such as eye-tracking, or in fields such as cybernetics or neural circuitry."/>
+                    <Page exact path="/contact" component={Contact} title="Contact - Reach out to us  if you have questions or comments. | mother.technology" description="It's an exciting time for neuroscience, and if you're excited about it too, we'd love to hear from you. Please drop us a line."/>
+                    <Page exact path="/privacy" component={Privacy} title="Privacy - We make strong promises on data security. | mother.technology" description="We value your data, to feedback into our models and optimise the tools we develop. We promise to treat it with care, keeping it anonymous, encrypted and deleteable."/>
                 </Switch>
             </div>
         </Router>
