60c70cccef4d04a4d3fa74ae2d25f3bbe8415115
Mikael Hultgren <blomma@artsoftheinsane.com>
2022-03-09 17:59:21 +0100

    Maybe

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,161 +1,177 @@
 /* eslint-disable jsx-a11y/alt-text */
 import React from "react";
-import "./Shared.css";
-import "./Main.css";
-import { Header } from "./Header";
-import { Panel } from "./Panel";
-import { PanelSellItem } from "./PanelSellItem";
 import { CircleWithText } from "./CircleWithText";
+import { Header } from "./Header";
+import "./Main.css";
+import { Panel } from "./Panel";
+import PanelSellItem from "./PanelSellItem";
+import "./Shared.css";
 import { USPList } from "./USPList";
 
 export const Main: React.FC = () => {
-  let usp = [
-    {
-      title: "Research-based",
-      text: "With our solution, you know that you get the real science, based on latest neuroscience papers.",
-    },
-    { title: "Title2", text: "Text2" },
-  ];
+    let usp = [
+        {
+            title: "Research-based",
+            text: "With our solution, you know that you get the real science, based on latest neuroscience papers.",
+        },
+        { title: "Title2", text: "Text2" },
+    ];
 
-  return (
-    <>
-      <div>
-        <Header
-          className="main-header"
-          header="Mother."
-          subHeader="Changing minds"
-        />
-        <div className="side-margin">
-          <div className="flex main">
-            <p>
-              We are a Stockholm-based hard-science deep-tech mental health
-              company, filling the niche between conventional health services
-              and self-help approaches by translating cutting edge neuroscience
-              into easily accessible digital therapeutics.
-            </p>
-            <p>
-              We create high-tech digital therapeutics based on Virtual Reality,
-              biofeedback, and guided mental training courses.
-            </p>
-          </div>
+    return (
+        <>
+            <div>
+                <Header
+                    className="main-header"
+                    header="Mother."
+                    subHeader="Changing minds"
+                />
+                <div className="side-margin">
+                    <div className="flex main">
+                        <p>
+                            We are a Stockholm-based hard-science deep-tech
+                            mental health company, filling the niche between
+                            conventional health services and self-help
+                            approaches by translating cutting edge neuroscience
+                            into easily accessible digital therapeutics.
+                        </p>
+                        <p>
+                            We create high-tech digital therapeutics based on
+                            Virtual Reality, biofeedback, and guided mental
+                            training courses.
+                        </p>
+                    </div>
 
-          <div className="flex panels">
-            <Panel header="The brain" backgroundColor="pink">
-              <>
-                <p className="small">The Brain, home to us all! </p>
-                <p className="small">
-                  Never has the research on our most thinky organ been so
-                  advanced, yet globally mental health plummets.{" "}
-                </p>
-                <p className="small">
-                  Anxiety, burnout, depersonalisation, depression, and mood
-                  disorders are all too common, and amongst the youth they're on
-                  the rise. These mental health issues highlight the unhappy
-                  state of our brains.
-                </p>
-              </>
-            </Panel>
-            <Panel header="The science" backgroundColor="yellow">
-              <>
-                <p className="small">
-                  In research labs across the world, using tools such as VR or
-                  psychedelics, in molecular biology labs, in fMRI studies, the
-                  cutting edge of science is revealing aspects of our brains
-                  which show us a clear path forward for brain health.
-                </p>
-                <p className="small">
-                  From this and our own in-house research we develop novel
-                  digital therapeutics targetting the brain.
-                </p>
-              </>
-            </Panel>
-            <Panel header="The target" backgroundColor="black">
-              <>
-                <p className="small">
-                  We aim to enable you to fully affirm your life, yourself, and
-                  your mode of functioning. We want your brain (and body)
-                  happily ticking along like a Swiss watch. We want you to feel
-                  like every day is the best day of your life, until tomorrow...
-                </p>
-                <p className="small">
-                  And we want you to know who you are, where you want to go, and
-                  how to get there.
-                </p>
-              </>
-            </Panel>
-          </div>
-          <div className="separator" />
-          <div className="flex main">
-            <USPList usp={usp} />
-          </div>
-          <div className="separator" />
-          <div className="flex main">
-            <h2>VR text</h2>
-            <p>
-              We believe the trends in neuroscience are revealing hidden aspects
-              of the brain, and that we are approaching a new dawn for digital
-              mental health therapeutics, and we aim to be at the forefront of
-              these developments.
-            </p>
-          </div>
-          <CircleWithText backgroundColor="pink">
-            <p>
-              We believe the trends in neuroscience are revealing hidden aspects
-              of the brain, and that we are approaching a new dawn for digital
-              mental health therapeutics, and we aim to be at the forefront of
-              these developments.
-            </p>
-          </CircleWithText>
-          <div className="separator" />
-          <div className="flex main">
-            <h2>Our other products</h2>
-            <p>What is in it for you</p>
-          </div>
-          <div className="flex panel-sell-items">
-            <PanelSellItem
-              header="Hertz."
-              img="/images/hz-square.png"
-              link="/hertz"
-              linkText="Read more about Hertz."
-              buttonText="DOWNLOAD NOW"
-              buttonLink="https://apps.apple.com/us/app/hertz-an-anxiety-treatment/id1529146685"
-            >
-              <>
-                <p className="small">
-                  Work on your vagus nerve. Became a more calm person.
-                </p>
-              </>
-            </PanelSellItem>
-            <PanelSellItem
-              header="Hertz lite."
-              img="/images/hz-lite-square.png"
-              link="/hertz-lite"
-              linkText="Read more about Hertz lite."
-              buttonText="DOWNLOAD NOW"
-              buttonLink="https://apps.apple.com/us/app/hertz-an-anxiety-treatment/id1529146685"
-            >
-              <>
-                <p className="small">
-                  For free. No ads. But still makes you pay.
-                </p>
-              </>
-            </PanelSellItem>
+                    <div className="flex panels">
+                        <Panel header="The brain" backgroundColor="pink">
+                            <>
+                                <p className="small">
+                                    The Brain, home to us all!{" "}
+                                </p>
+                                <p className="small">
+                                    Never has the research on our most thinky
+                                    organ been so advanced, yet globally mental
+                                    health plummets.{" "}
+                                </p>
+                                <p className="small">
+                                    Anxiety, burnout, depersonalisation,
+                                    depression, and mood disorders are all too
+                                    common, and amongst the youth they're on the
+                                    rise. These mental health issues highlight
+                                    the unhappy state of our brains.
+                                </p>
+                            </>
+                        </Panel>
+                        <Panel header="The science" backgroundColor="yellow">
+                            <>
+                                <p className="small">
+                                    In research labs across the world, using
+                                    tools such as VR or psychedelics, in
+                                    molecular biology labs, in fMRI studies, the
+                                    cutting edge of science is revealing aspects
+                                    of our brains which show us a clear path
+                                    forward for brain health.
+                                </p>
+                                <p className="small">
+                                    From this and our own in-house research we
+                                    develop novel digital therapeutics
+                                    targetting the brain.
+                                </p>
+                            </>
+                        </Panel>
+                        <Panel header="The target" backgroundColor="black">
+                            <>
+                                <p className="small">
+                                    We aim to enable you to fully affirm your
+                                    life, yourself, and your mode of
+                                    functioning. We want your brain (and body)
+                                    happily ticking along like a Swiss watch. We
+                                    want you to feel like every day is the best
+                                    day of your life, until tomorrow...
+                                </p>
+                                <p className="small">
+                                    And we want you to know who you are, where
+                                    you want to go, and how to get there.
+                                </p>
+                            </>
+                        </Panel>
+                    </div>
+                    <div className="separator" />
+                    <div className="flex main">
+                        <USPList usp={usp} />
+                    </div>
+                    <div className="separator" />
+                    <div className="flex main">
+                        <h2>VR text</h2>
+                        <p>
+                            We believe the trends in neuroscience are revealing
+                            hidden aspects of the brain, and that we are
+                            approaching a new dawn for digital mental health
+                            therapeutics, and we aim to be at the forefront of
+                            these developments.
+                        </p>
+                    </div>
+                    <CircleWithText backgroundColor="pink">
+                        <p>
+                            We believe the trends in neuroscience are revealing
+                            hidden aspects of the brain, and that we are
+                            approaching a new dawn for digital mental health
+                            therapeutics, and we aim to be at the forefront of
+                            these developments.
+                        </p>
+                    </CircleWithText>
+                    <div className="separator" />
+                    <div className="flex main">
+                        <h2>Our other products</h2>
+                        <p>What is in it for you</p>
+                    </div>
+                    <div className="flex panel-sell-items">
+                        <PanelSellItem
+                            header="Hertz."
+                            img="/images/hz-square.png"
+                            link="/hertz"
+                            linkText="Read more about Hertz."
+                            buttonText="DOWNLOAD NOW"
+                            buttonLink="https://apps.apple.com/us/app/hertz-an-anxiety-treatment/id1529146685"
+                        >
+                            <>
+                                <p className="small">
+                                    Work on your vagus nerve. Became a more calm
+                                    person.
+                                </p>
+                            </>
+                        </PanelSellItem>
+                        <PanelSellItem
+                            header="Hertz lite."
+                            img="/images/hz-lite-square.png"
+                            link="/hertz-lite"
+                            linkText="Read more about Hertz lite."
+                            buttonText="DOWNLOAD NOW"
+                            buttonLink="https://apps.apple.com/us/app/hertz-an-anxiety-treatment/id1529146685"
+                        >
+                            <>
+                                <p className="small">
+                                    For free. No ads. But still makes you pay.
+                                </p>
+                            </>
+                        </PanelSellItem>
 
-            <PanelSellItem
-              header="Self-confidence."
-              img="/images/hz-square.png"
-              link="/self-confidence"
-              linkText="Read more about our coming app."
-            >
-              <>
-                <p className="small">Get better self confidence.</p>
-              </>
-            </PanelSellItem>
-          </div>
-        </div>
-      </div>
-    </>
-  );
+                        <PanelSellItem
+                            header="Self-confidence."
+                            img="/images/hz-square.png"
+                            link="/self-confidence"
+                            linkText="Read more about our coming app."
+                        >
+                            <>
+                                <p className="small">
+                                    Get better self confidence.
+                                </p>
+                            </>
+                        </PanelSellItem>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 };
 
 // header: string;
--- src/Menu.tsx
+++ src/Menu.tsx
@@ -1,101 +1,114 @@
 /* eslint-disable jsx-a11y/alt-text */
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { useState } from "react";
-import "./Menu.css";
-import { Link, withRouter, RouteComponentProps } from "react-router-dom";
-import { HashLink } from "react-router-hash-link";
 import { IconContext } from "react-icons";
-import { IoMenuOutline, IoCloseOutline } from "react-icons/io5";
+import { IoCloseOutline, IoMenuOutline } from "react-icons/io5";
+import { Link, RouteComponentProps, withRouter } from "react-router-dom";
+import "./Menu.css";
 
 export const Menu: React.FC<RouteComponentProps> = (props) => {
-  const [open, setOpen] = useState<boolean>(false);
-  const height = document.body.clientHeight;
-  const width = document.body.clientWidth;
-  let right: string = "25px";
+    const [open, setOpen] = useState<boolean>(false);
+    const height = document.body.clientHeight;
+    const width = document.body.clientWidth;
+    let right: string = "25px";
 
-  return (
-    <>
-      <div
-        id="menuIcon"
-        style={{ right: right }}
-        onClick={() => setOpen(!open)}
-      >
-        <IconContext.Provider value={{ className: "menuIcon" }}>
-          {open ? <IoCloseOutline /> : <IoMenuOutline />}
-        </IconContext.Provider>
-      </div>
-      {open !== false && (
-        <div
-          id="navbar"
-          className="flex"
-          style={{ height: height }}
-          onClick={() => setOpen(false)}
-        >
-          <div className="flex column">
-            <Link
-              to="/"
-              className={props.location.pathname === "/" ? "active" : ""}
+    return (
+        <>
+            <div
+                id="menuIcon"
+                style={{ right: right }}
+                onClick={() => setOpen(!open)}
             >
-              Home
-            </Link>
-            <Link to="/" className="subLink">
-              VR
-            </Link>
-            <Link to="/hertz" className="subLink">
-              Hertz
-            </Link>
-            <Link to="/hertz-lite" className="subLink">
-              Hertz lite
-            </Link>
-            <Link to="/self-confidence" className="subLink">
-              Self-confidence
-            </Link>
-            {/* <Link to="/#cognitive-tools" className="subLink">
+                <IconContext.Provider value={{ className: "menuIcon" }}>
+                    {open ? <IoCloseOutline /> : <IoMenuOutline />}
+                </IconContext.Provider>
+            </div>
+            {open !== false && (
+                <div
+                    id="navbar"
+                    className="flex"
+                    style={{ height: height }}
+                    onClick={() => setOpen(false)}
+                >
+                    <div className="flex column">
+                        <Link
+                            to="/"
+                            className={
+                                props.location.pathname === "/" ? "active" : ""
+                            }
+                        >
+                            Home
+                        </Link>
+                        <Link to="/" className="subLink">
+                            VR
+                        </Link>
+                        <Link to="/hertz" className="subLink">
+                            Hertz
+                        </Link>
+                        <Link to="/hertz-lite" className="subLink">
+                            Hertz lite
+                        </Link>
+                        <Link to="/self-confidence" className="subLink">
+                            Self-confidence
+                        </Link>
+                        {/* <Link to="/#cognitive-tools" className="subLink">
               Cognitive tools
             </Link> */}
-            {/* <Link to="/#biofeedback" className="subLink">
+                        {/* <Link to="/#biofeedback" className="subLink">
               Biofeedback
             </Link> */}
-            {/* <Link to="/#virtual-reality" className="subLink">
+                        {/* <Link to="/#virtual-reality" className="subLink">
               Virtual Reality
             </Link> */}
-            <Link
-              to="/team"
-              className={props.location.pathname === "/team" ? "active" : ""}
-            >
-              Team
-            </Link>
-            <Link to="team" className="subLink">
-              Our values
-            </Link>
-            <Link to="team" className="subLink">
-              Our interests
-            </Link>
-            <Link
-              to="/media"
-              className={props.location.pathname === "/media" ? "active" : ""}
-            >
-              Press
-            </Link>
-            {/* <Link to="team/#our-values" className="subLink">
+                        <Link
+                            to="/team"
+                            className={
+                                props.location.pathname === "/team"
+                                    ? "active"
+                                    : ""
+                            }
+                        >
+                            Team
+                        </Link>
+                        <Link to="team" className="subLink">
+                            Our values
+                        </Link>
+                        <Link to="team" className="subLink">
+                            Our interests
+                        </Link>
+                        <Link
+                            to="/media"
+                            className={
+                                props.location.pathname === "/media"
+                                    ? "active"
+                                    : ""
+                            }
+                        >
+                            Press
+                        </Link>
+                        {/* <Link to="team/#our-values" className="subLink">
               Our values
             </Link>
             <Link to="team/#our-interests" className="subLink">
               Our interests
             </Link> */}
-            <Link
-              to="/privacy"
-              className={props.location.pathname === "/privacy" ? "active" : ""}
-            >
-              Privacy
-            </Link>
-            <Link to="/privacy" className="subLink">
-              The small print
-            </Link>
-            {/* <Link to="/privacy#the-small-print" className="subLink">
+                        <Link
+                            to="/privacy"
+                            className={
+                                props.location.pathname === "/privacy"
+                                    ? "active"
+                                    : ""
+                            }
+                        >
+                            Privacy
+                        </Link>
+                        <Link to="/privacy" className="subLink">
+                            The small print
+                        </Link>
+                        {/* <Link to="/privacy#the-small-print" className="subLink">
               The small print
             </Link> */}
-            {/* <HashLink
+                        {/* <HashLink
                 to="/#cognitive-tools"
                 className="subLink"
               >
@@ -139,11 +152,11 @@ export const Menu: React.FC<RouteComponentProps> = (props) => {
               >
                 The small print
               </HashLink> */}
-          </div>
-        </div>
-      )}
-    </>
-  );
+                    </div>
+                </div>
+            )}
+        </>
+    );
 };
 
 export default withRouter(Menu);
