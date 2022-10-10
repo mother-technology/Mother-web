fa92708202642596003a34d9c531900e1482d61a
tomasmckenna <t@mckenna.im>
2022-10-10 09:19:52 +0200

    Menu horizontal

--- src/Menu.tsx
+++ src/Menu.tsx
@@ -32,46 +32,47 @@ export const Menu: React.FC = () => {
                     style={{ height: height }}
                     onClick={() => setOpen(false)}
                 >
-                    <div className="flex column">
-                        <Link
-                            to="/"
-                            className={
-                                location.pathname === "/" ? "active" : ""
-                            }
-                        >
-                            Home
-                        </Link>
-                        <HashLink
-                            to="/#sick-of-smoking"
-                            className={
-                                location.hash === "#sick-of-smoking"
-                                    ? "subLink active"
-                                    : "subLink"
-                            }
-                        >
-                            Sick of Smoking
-                        </HashLink>
-                        <HashLink
-                            to="/#who-we-are"
-                            className={
-                                location.hash === "#who-we-are"
-                                    ? "subLink active"
-                                    : "subLink"
-                            }
-                        >
-                            Who we are
-                        </HashLink>
-                        <HashLink
-                            to="/#news"
-                            className={
-                                location.hash === "#news"
-                                    ? "subLink active"
-                                    : "subLink"
-                            }
-                        >
-                            News
-                        </HashLink>
-                        {/* <Link
+                    <div className="flex nav-container">
+                        <div className="link-group">
+                            <Link
+                                to="/"
+                                className={
+                                    location.pathname === "/" ? "active" : ""
+                                }
+                            >
+                                Home
+                            </Link>
+                            <HashLink
+                                to="/#sick-of-smoking"
+                                className={
+                                    location.hash === "#sick-of-smoking"
+                                        ? "subLink active"
+                                        : "subLink"
+                                }
+                            >
+                                Sick of Smoking
+                            </HashLink>
+                            <HashLink
+                                to="/#who-we-are"
+                                className={
+                                    location.hash === "#who-we-are"
+                                        ? "subLink active"
+                                        : "subLink"
+                                }
+                            >
+                                Who we are
+                            </HashLink>
+                            <HashLink
+                                to="/#news"
+                                className={
+                                    location.hash === "#news"
+                                        ? "subLink active"
+                                        : "subLink"
+                                }
+                            >
+                                News
+                            </HashLink>
+                            {/* <Link
                             to="/vr-for-health-tech"
                             className={
                                 location.pathname === "vr-for-health-tech"
@@ -81,50 +82,64 @@ export const Menu: React.FC = () => {
                         >
                             VR for health tech
                         </Link> */}
-                        <Link
-                            to="/team"
-                            className={
-                                location.pathname === "/team" ? "active" : ""
-                            }
-                        >
-                            Team
-                        </Link>
-                        <HashLink
-                            to="/team#our-values"
-                            className={
-                                location.hash === "#our-values"
-                                    ? "subLink active"
-                                    : "subLink"
-                            }
-                        >
-                            Our values
-                        </HashLink>
-                        <Link
-                            to="/media"
-                            className={
-                                location.pathname === "/media" ? "active" : ""
-                            }
-                        >
-                            Media
-                        </Link>
-                        <Link
-                            to="/privacy"
-                            className={
-                                location.pathname === "/privacy" ? "active" : ""
-                            }
-                        >
-                            Privacy
-                        </Link>
-                        <HashLink
-                            to="/privacy#how-we-protect-your-data"
-                            className={
-                                location.hash === "#how-we-protect-your-data"
-                                    ? "subLink active"
-                                    : "subLink"
-                            }
-                        >
-                            How we protect your data
-                        </HashLink>
+                        </div>
+                        <div className="link-group">
+                            <Link
+                                to="/team"
+                                className={
+                                    location.pathname === "/team"
+                                        ? "active"
+                                        : ""
+                                }
+                            >
+                                Team
+                            </Link>
+                            <HashLink
+                                to="/team#our-values"
+                                className={
+                                    location.hash === "#our-values"
+                                        ? "subLink active"
+                                        : "subLink"
+                                }
+                            >
+                                Our values
+                            </HashLink>
+                        </div>
+                        <div className="link-group">
+                            <Link
+                                to="/media"
+                                className={
+                                    location.pathname === "/media"
+                                        ? "active"
+                                        : ""
+                                }
+                            >
+                                Media
+                            </Link>
+                        </div>
+                        <div className="link-group">
+                            <Link
+                                to="/privacy"
+                                className={
+                                    location.pathname === "/privacy"
+                                        ? "active"
+                                        : ""
+                                }
+                            >
+                                Privacy
+                            </Link>
+                            <HashLink
+                                to="/privacy#how-we-protect-your-data"
+                                className={
+                                    location.hash ===
+                                    "#how-we-protect-your-data"
+                                        ? "subLink active"
+                                        : "subLink"
+                                }
+                            >
+                                How we protect your data
+                            </HashLink>
+                        </div>
                     </div>
                 </div>
             )}
